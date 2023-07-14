290290710b4a68e91e43987d8b10da79072fbc2c
HATAth <hata@throo.io>
2023-07-14 16:33:30 +0900

    exericise_v2_first

--- exercise1.rb
+++ exercise1.rb
@@ -2,6 +2,14 @@ print "あなたの年齢は？:"
 age = gets.chomp
 age = age.to_i
 
+print "クーポンはありますか？ (y/n) :"
+coupon = gets.chomp
+if(coupon == "y")
+    discount = 200
+else
+    discount = 0
+end
+
 if age == 60
     fee = 10
 elsif age > 25
@@ -11,5 +19,8 @@ elsif age >= 20
 else
     fee = 200
 end
+
+fee -= discount
+fee = [0, fee].max  #負の値ならば0円にする
 
 puts "あなたの料金は#{fee}円です"
