afba9e475b8106af99b69128f0d46540b3d4e66e
HATAth <hata@throo.io>
2023-07-24 17:20:54 +0900

    exercise4_v3_first

--- exercise4.rb
+++ exercise4.rb
@@ -11,10 +11,10 @@ response = Faraday.get("https://next-chat-kohl.vercel.app/api/room_ids")
 # response.bodyはJSON文字列なので、JSON.parseで文字列からrubyのハッシュに変換する
 room_ids = JSON.parse(response.body)
 
-puts room_ids
 #求めたハッシュroom_idsのキーroomIdsに対応する部屋の数を求める
 numOfRooms = room_ids["roomIds"].size
 
+ 
 #それぞれの部屋の投稿をrubyのhashに変換し、配列postsに追加していく
 posts = []
 for i in 1..numOfRooms
@@ -22,10 +22,34 @@ for i in 1..numOfRooms
     posts.push(JSON.parse(responsePost.body))
 end
 
-#得た各部屋の投稿をそれぞれ表示
-#コマンドに渡された引数が"rooms"ならばこの部分は実行しない
-if ARGV[0] != "rooms"
+
+
+#コマンドに渡された引数で、表示するフォーマットを指定するフラグの値を決める
+if ARGV[0] == "rooms"
+    formatFlag = 1
+elsif ARGV[0] == "posts"
+    formatFlag = 2
+    for i in 1..numOfRooms
+        if "room-#{i}" == ARGV[1]
+            putPostRoom = i
+        end
+    end
+else
+    formatFlag = 0
+end
+
+case formatFlag
+when 0 then
+    #部屋の一覧を表示
+    puts room_ids
+    #得た各部屋の投稿をそれぞれ表示
     posts.each do |post|
         puts post
     end
+when 1 then
+    puts room_ids
+when 2 then
+    #コマンドに渡された部屋の投稿のみを表示
+    puts posts[putPostRoom - 1]
 end
+   
